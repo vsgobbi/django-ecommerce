9a2870e0dc0d102329c5d809698c8a81ce189dec
ongui <vitorsgobbi@hotmail.com>
2020-03-27 23:42:55 -0300

    refactor cart model and form

--- ecommerce/cart/forms.py
+++ ecommerce/cart/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-
+from django.utils.translation import ugettext_lazy as translate
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 26)]
 
 
 class CartAddProductForm(forms.Form):
-    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(label=translate("Quantidade"), choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
