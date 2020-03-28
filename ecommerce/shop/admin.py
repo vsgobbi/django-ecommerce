a83cf260d2ed8d7ee99a1715fcabeaacb708c686
ongui <vitorsgobbi@hotmail.com>
2020-03-28 00:50:35 -0300

    refactor templates

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -6,26 +6,26 @@ logger = logging.getLogger(__name__)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
 
 
 try:
     admin.site.register(Category, CategoryAdmin)
     logger.info("Nova categoria criada %s", Category)
-except:
-    logger.exception("Nao foi possivel criar categoria")
+except Exception as e:
+    logger.exception("Não foi possével criar categoria: {}".format(e))
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'available', 'created_at', 'updated_at']
-    list_filter = ['available', 'created_at', 'updated_at']
-    list_editable = ['price', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ["name", "slug", "price", "available", "created_at", "updated_at"]
+    list_filter = ["available", "created_at", "updated_at"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug": ("name",)}
 
 
 try:
     admin.site.register(Product, ProductAdmin)
     logger.info("Novo produto criado %s", Product)
-except:
-    logger.exception("Nao foi possivel criar produto")
+except Exception as e:
+    logger.exception("Não foi possével criar produto, {}".format(e))
--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,43 +1,56 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import ugettext_lazy as translate
+
+RATING = [
+    ("S", translate("Super")),
+    ("M", translate("Muito")),
+    ("P", translate("Pouco")),
+    ("N", translate("Nenhuma")),
+]
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, db_index=True)
+    name = models.CharField(translate("Nome"), max_length=150, db_index=True)
     slug = models.SlugField(max_length=150, unique=True, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ('name', )
-        verbose_name = 'category'
-        verbose_name_plural = 'categories'
+        ordering = ("name", )
+        verbose_name = "category"
+        verbose_name_plural = "categories"
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
+        return reverse("shop:product_list_by_category", args=[self.slug])
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, db_index=True)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(translate("Nome"), max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    available = models.BooleanField(default=True)
-    stock = models.PositiveIntegerField()
+    cart_added = models.PositiveIntegerField(editable=False, null=True)
+    description = models.TextField(translate("Descrição"), blank=True)
+    last_price = models.DecimalField(translate("Valor anterior"), max_digits=10, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(translate("Valor atual"), max_digits=10, decimal_places=2,  null=False)
+    available = models.BooleanField(translate("Disponível"), default=True)
+    stock = models.PositiveIntegerField(translate("Quantidade em estoque"), default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(translate("Imagem"), upload_to="products/%Y/%m/%d", blank=True)
 
     class Meta:
-        ordering = ('name', )
-        index_together = (('id', 'slug'),)
+        ordering = ("name", "available", "price")
+        index_together = (("id", "slug"),)
 
     def __str__(self):
         return self.name
 
+    def show_desc(self):
+        return self.description[:75]
+
     def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
+        return reverse("shop:product_detail", args=[self.id, self.slug])
